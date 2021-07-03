28fc8dc3417d86ad465baa763734a4f2cee8fcef
Rodrigo Makoto <makoto.rodrigo@gmail.com>
2021-07-03 03:55:05 -0300

    Fixed install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 if [ ! -d "$HOME/.dotfiles" ]; then
     echo "Installing dotfiles for the first time"
     mkdir $HOME/.dotfiles
-    git clone --recurse-submodules -j8 --depth=1 https://github.com/rmakoto/my-dotfiles.git "$HOME/.dotfiles"
+    git clone --recurse-submodules -j8 git@github.com:rmakoto/dotfiles.git "$HOME/.dotfiles"
     cd "$HOME/.dotfiles"
 
     bash /$HOME/.dotfiles/installer/macos/setup.sh
