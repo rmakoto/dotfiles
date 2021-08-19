8585df6ac85b679227388b23814d3525f3f7dc2f
Rodrigo Makoto <makoto.rodrigo@gmail.com>
2021-08-19 14:56:17 -0300

    Updated terraform zsh plugin

--- zsh/modules/terraform.zsh
+++ zsh/modules/terraform.zsh
@@ -1,3 +1,55 @@
+# https://github.com/macunha1/zsh-terraform/blob/master/terraform.zsh
 # Terraform autocompletion
 autoload -U +X bashcompinit && bashcompinit
-complete -o nospace -C /usr/local/bin/terraform terraform
+complete -o nospace -C $(which terraform) terraform
+
+tf_prompt_info() {
+	# dont show 'default' workspace in home dir
+	[[ "$PWD" == ~ ]] && return
+	# check if in terraform dir
+	if [ -d .terraform ]; then
+		workspace=$(terraform workspace show 2>/dev/null) || return
+		echo "[${workspace}]"
+	fi
+}
+
+# If you notice, aliases are bounded to each other
+# Why? Gives the freedom to override the lower levels and to affect all of them
+# e.g.: alias tfi=terraform init -no-color -reconfigure
+
+alias tf=terraform
+
+alias tfi='tf init'
+
+alias tfp='tf plan'
+alias tfip='tfi && tfp'
+
+alias tfa='tf apply'
+alias tfia='tfi && tfa'
+
+alias tfd='tf destroy'
+alias tfid='tfi && tfd'
+
+# DANGER zone
+alias tfa!='tfa -auto-approve'
+alias tfia!='tfi && tfa!'
+# DANGER++!!
+alias tfd!='tfd -auto-approve'
+alias tfid!='tfi && tfd!'
+
+alias tfc='tf console'
+alias tfg='tf graph'
+alias tfc='tf console'
+alias tfget='tf get'
+alias tfimp='tf import'
+alias tfo='tf output'
+alias tfprov='tf providers'
+alias tfpp='tf push'
+alias tfr='tf refresh'
+alias tfs='tf show'
+alias tfst='tf state'
+alias tft='tf taint'
+alias tfunt='tf untaint'
+alias tfv='tf validate'
+alias tfver='tf version'
+alias tfw='tf workspace'
