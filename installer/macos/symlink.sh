a3d59b67e3ccf84cec411e2adabdf1573b019df2
Rodrigo Makoto <makoto.rodrigo@gmail.com>
2022-05-17 18:45:34 -0300

    Updated intaller and gitconfig

--- installer/macos/symlink.sh
+++ installer/macos/symlink.sh
@@ -2,13 +2,13 @@
 
 echo "Creating symlinks..."
 
-export DOTFILES_DIR=$HOME/.dotfiles
+export DOTFILES=$HOME/.dotfiles
 
 # zsh
-ln -sf "$DOTFILES_DIR/zsh/zlogin" "$HOME/.zlogin"
-ln -sf "$DOTFILES_DIR/zsh/zshrc" "$HOME/.zshrc"
-ln -sf "$DOTFILES_DIR/zsh/zshenv" "$HOME/.zshenv"
-ln -sf "$DOTFILES_DIR/zsh/zprofile" "$HOME/.zprofile"
+ln -sf "$DOTFILES/zsh/zlogin" "$HOME/.zlogin"
+ln -sf "$DOTFILES/zsh/zshrc" "$HOME/.zshrc"
+ln -sf "$DOTFILES/zsh/zshenv" "$HOME/.zshenv"
+ln -sf "$DOTFILES/zsh/zprofile" "$HOME/.zprofile"
 
 # zsh modules
 zsh_fpath="$HOME/.zfunctions"
@@ -20,27 +20,27 @@ zsh_modules="$DOTFILES/zsh/modules"
 # ln -s "$zsh_modules/z/z.1" "$man1_path/z.1"
 
 # vim
-# ln -sf "$DOTFILES_DIR/vim/vimrc" "$HOME/.vimrc"
-# ln -sf "$DOTFILES_DIR/vim/gvimrc" "$HOME/.gvimrc"
+# ln -sf "$DOTFILES/vim/vimrc" "$HOME/.vimrc"
+# ln -sf "$DOTFILES/vim/gvimrc" "$HOME/.gvimrc"
 # mkdir "$HOME/.vim/autoload"
 # ln -sf "$DOTFILES/vim/autoload/*" "$HOME/.vim/autoload/"
 # mkdir "$HOME/.vim/after"
-# ln -sf "$DOTFILES_DIR/vim/ftplugin" "$HOME/.vim/after/ftplugin"
+# ln -sf "$DOTFILES/vim/ftplugin" "$HOME/.vim/after/ftplugin"
 # mkdir "$HOME/.vim/snippets"
-# ln -sf "$DOTFILES_DIR/vim/snippets" "$HOME/.vim/snippets"
+# ln -sf "$DOTFILES/vim/snippets" "$HOME/.vim/snippets"
 
 # tmux
-ln -sf "$DOTFILES_DIR/tmux/tmux.conf" "$HOME/.tmux.conf"
+ln -sf "$DOTFILES/tmux/tmux.conf" "$HOME/.tmux.conf"
 
 # git
-ln -sf "$DOTFILES_DIR/git/gitconfig" "$HOME/.gitconfig"
-ln -sf "$DOTFILES_DIR/git/gitignore" "$HOME/.gitignore"
+ln -sf "$DOTFILES/git/gitconfig" "$HOME/.gitconfig"
+ln -sf "$DOTFILES/git/gitignore" "$HOME/.gitignore"
 
 # TheSilverSearcher ignore list
-ln -sf "$DOTFILES_DIR/ag/agignore" "$HOME/.agignore"
+ln -sf "$DOTFILES/ag/agignore" "$HOME/.agignore"
 
 # ctags
-# ln -sf "$DOTFILES_DIR/ctags/ctags" "$HOME/.ctags"
+# ln -sf "$DOTFILES/ctags/ctags" "$HOME/.ctags"
 
 # curl
 # ln -sf "$DOTFILES/curl/curlrc" "$HOME/.curlrc"
@@ -50,9 +50,9 @@ if [ ! -d "$HOME/.config" ]
 then
     mkdir -p $HOME/.config
 fi
-ln -sf "$DOTFILES_DIR/starship-prompt/starship.toml" "$HOME/.config/starship.toml"
+ln -sf "$DOTFILES/starship-prompt/starship.toml" "$HOME/.config/starship.toml"
 
 # Add dynamic profile in iterm2
-ln -sf "$DOTFILES_DIR/iterm2/rmakoto-profile.json" "$HOME/Library/Application Support/iTerm2/DynamicProfiles/rmakoto-profile.json"
+ln -sf "$DOTFILES/iterm2/rmakoto-profile.json" "$HOME/Library/Application Support/iTerm2/DynamicProfiles/rmakoto-profile.json"
 
 echo "Done."
